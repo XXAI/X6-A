9381acdacffb45ec05685341ec414b058101625f
Mario Alberto Villatoro <ing.mario.villatoro@gmail.com>
2018-04-12 23:53:12 -0500

    Actualizacion de blade welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">SEGUIMIENTO (DIPRIS)</div>
+                <div class="title"> (TECPRIS)</div>
             </div>
         </div>
     </body>
